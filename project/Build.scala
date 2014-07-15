3859d18d9297a2c1282503cf6d40d7672fb94540
Matthew Neeley <mneeley@gmail.com>
2014-07-15 06:21:53 -1000

    Bump version to 0.1.4

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import bintray.Plugin._
 import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 
 object BuildSettings {
-  val buildVersion = "0.1.3"
+  val buildVersion = "0.1.4"
   val buildScalaVersion = "2.11.1"
   val buildScalaOrganization = "org.scala-lang"
 
